f9c07b6ce646ddc4c6b7375d49a775ef3efaa059
Sydney <sbauer@andrew.cmu.edu>
2018-11-16 15:29:16 -0500

    game, location, sport models created along with their views, migrations, tests, and controllers b/c used scaffold in creation

---
+++ app/models/game.rb
@@ -0,0 +1,41 @@
+class Game < ApplicationRecord
+
+  # Relationships
+  # -------------------
+  belongs_to :location
+  belongs_to :sport
+
+  has_many :users, through: :user_games
+  has_one :location
+
+  # Scopes
+  # --------------------
+  scope :for_location, -> (location_id) { where("location_id = ?", location_id) }
+  scope :for_sport, -> (sport_id) { where("sport_id = ?", sport_id) }
+  scope :sort_time, -> { order('time') }
+  scope :search, -> (term) { where("description LIKE ?", "%#{term}%")}
+
+
+  # Validations
+  # --------------------
+  validates_presence_of :time, :max_size, :location_id, :sport_id
+  validates :description, allow_blank: true
+
+  validates_numericality_of :max_size, only_integer: true, greater_than: 0
+
+  validates_numericality_of :curr_size, only_integer: true, greater_than: 0, allow_blank: true
+
+
+  # Misc Methods and Constants
+  # --------------------
+  def is_full
+    return self.curr_size == self.max_size
+  end
+
+  #def sort by location?
+
+  # Use private methosd to execute the custom validations
+  # ---------------------------
+  private
+
+end
