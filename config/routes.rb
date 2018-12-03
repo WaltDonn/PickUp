c47691ef0344f1174d58f6fec239491f6e699cd2
Walter Donnellan <Walt@Walters-MBP.wv.cc.cmu.edu>
2018-12-02 22:41:06 -0500

    basic login/reg steps setup

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,6 @@ Rails.application.routes.draw do
   resources :locations
   resources :games
   resources :sports
+  get "/auth/:page" => "auth#show"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
